8ca328eff3792d5121d59448505739fe0f18f142
Kunal2341 <kunal.aneja101@gmail.com>
2022-10-07 21:53:37 -0400

    fixing frame interval for scanning qr code

--- QRCodeScanner.py
+++ QRCodeScanner.py
@@ -6,8 +6,8 @@ def sorted_alphanumeric(data):
     convert = lambda text: int(text) if text.isdigit() else text.lower()
     alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9]+)', key) ] 
     return sorted(data, key=alphanum_key)
-videoFileName = "ScanQRCodeTest.MOV"
-FRAME_INTERVAL = 500
+videoFileName = "ScanQRCodeShort.mp4"
+FRAME_INTERVAL = 1
 
 vidcap = cv2.VideoCapture(videoFileName)
 fps = vidcap.get(cv2.CAP_PROP_FPS)      # OpenCV2 version 2 used "CV_CAP_PROP_FPS"
@@ -18,17 +18,17 @@ folderName = "./"+os.path.splitext(videoFileName)[0]
 if not os.path.exists(folderName):
     os.mkdir(folderName)
 os.chdir(folderName)
-"""
+
 success,image = vidcap.read()
 count = 0
 while success:
-    vidcap.set(cv2.CAP_PROP_POS_MSEC,(count*100))
+    vidcap.set(cv2.CAP_PROP_POS_MSEC,(count*(1000/(fps*FRAME_INTERVAL))))
     cv2.imwrite("Frame%d.jpg" % count, image)     # save frame as JPEG file      
     print("Saved {}".format(count))
     success,image = vidcap.read()
     #print('Read a new frame: ', success)
     count += 1
-"""
+
 #ScanQRCodeTest.MOV
 hasValueCount = 0
 for imageFrame in sorted_alphanumeric(os.listdir(".")):
@@ -39,10 +39,15 @@ for imageFrame in sorted_alphanumeric(os.listdir(".")):
         print(imageFrame + "\t-\t" + value)
         hasValueCount+=1
 
+checkingFrameCt = round(duration*1000/(fps*FRAME_INTERVAL))
+print("-"*40)
+print("Frame Rate:\t\t\t{}\nDuration:\t\t\t{} sec".format(round(fps, 5), duration))
+print("TotalFrames:\t\t\t{}".format(round(duration*1000/fps)))
+print("Number of frames checking:\t{} (Saving every {} frame)".format(checkingFrameCt, FRAME_INTERVAL))
+print("Positive Scans:\t\t\t{}\nPercantage Working:\t\t{}%".format(hasValueCount, round(hasValueCount/checkingFrameCt,2)*100)) #out of checking
+print("-"*40)
 
-checkingFrameCt = round(FRAME_INTERVAL/1000*fps*duration) #Fix this
-print("-"*40)
-print("Frame Rate:\t{}\nDuration:\t{} sec\nTotalFrames:\t{}".format(round(fps, 3), duration, round(fps*duration)))
-print("Checking Total Frames:\t{} (Saving every {} frame)".format(checkingFrameCt, FRAME_INTERVAL/1000))
-print("Positive Scans:\t{}\nPercantage Working:\t{}".format(hasValueCount, round(hasValueCount/checkingFrameCt*100)))
-print("-"*40)
+
+#print("Frame Rate:\t\t{}\nDuration:\t\t{} sec\nTotalFrames:\t\t{}".format(round(fps, 5), duration, round(fps*duration)))
+#print("Checking Total Frames:\t{} (Saving every {} frame)".format(checkingFrameCt, FRAME_INTERVAL/1000))
+
